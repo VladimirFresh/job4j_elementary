669a9d0e02508f8f05a8e762861f05a8b23fcd2f
Vladimir Vavilin <vavilinva59@gmail.com>
2022-05-19 12:42:19 +0500

    5.5. Циклический сдвиг вправо

---
+++ src/main/java/ru/job4j/array/RightShift.java
@@ -0,0 +1,22 @@
+package ru.job4j.array;
+
+public class RightShift {
+
+    public static void shift(int[] nums, int count) {
+            int[] newArray = new int[nums.length];
+            System.arraycopy(nums, 0, newArray, 0, nums.length);
+            System.arraycopy(newArray, 0, nums, count, nums.length - count);
+            System.arraycopy(newArray, nums.length - count, nums, 0, count);
+
+    }
+
+    // метод делает сдвиг с шагом 1
+    private static void shift(int[] nums) {
+        int[] newArray = new int[nums.length];
+        System.arraycopy(nums, 0, newArray, 0, nums.length);
+        System.arraycopy(newArray, 0, nums, 1, nums.length - 1);
+        System.arraycopy(newArray, nums.length - 1, nums, 0, 1);
+
+    }
+
+}
