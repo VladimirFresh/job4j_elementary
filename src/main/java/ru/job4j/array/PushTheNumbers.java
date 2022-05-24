0d51a2d41e97eb3d73e5c1eacf58eb36b4f34bd8
Vladimir Vavilin <vavilinva59@gmail.com>
2022-05-24 15:23:39 +0500

    6.4. Раздвинуть элементы массива

---
+++ src/main/java/ru/job4j/array/PushTheNumbers.java
@@ -0,0 +1,39 @@
+package ru.job4j.array;
+
+import java.util.Arrays;
+
+public class PushTheNumbers {
+
+    public static void push(int[][] array, int row, int column) {
+        int[][] result = new int[array.length][array.length];
+        for (int i = 0; i <= row; i++) {
+            for (int j = 0; j < array[i].length; j++) {
+                if (i == row && j == column) {
+                    break;
+                } else if (i < row) {
+                    if (j == column) {
+                        array[i][j] = array[i + 1][j];
+                    }
+                } else {
+                    array[i][j] = array[i][j + 1];
+                    }
+
+                }
+            }
+        for (int i = array.length - 1; i >= row; i--) {
+            for (int j = array[i].length - 1; j >= 0; j--) {
+                if (i == row && j == column) {
+                    break;
+                } else if (i > row) {
+                    if (j == column) {
+                        array[i][j] = array[i - 1][j];
+                    }
+                } else {
+                    array[i][j] = array[i][j - 1];
+                }
+            }
+        }
+        array[row][column] = 0;
+
+    }
+}
