f017e411f1de903dd28f2d9b22e11db5b54400a9
Vladimir Vavilin <vavilinva59@gmail.com>
2023-05-22 12:49:55 +0500

    14. Экранирование символов. Управляющие последовательности. [#504916]

---
+++ src/main/java/ru/job4j/EscapeExample.java
@@ -0,0 +1,8 @@
+package ru.job4j;
+
+public class EscapeExample {
+    public static void main(String[] args) {
+        String str = "Я изучаю на \"Job4j\" экранирование символов";
+        System.out.println(str);
+    }
+}
